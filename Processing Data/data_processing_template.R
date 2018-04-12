b40b76ce1157ccd3d29c025441a66266859b48b0
krawat428 <krawat428@gmail.com>
2018-04-12 19:18:35 +0530

    Create data_processing_template.R

---
+++ Processing Data/data_processing_template.R
@@ -0,0 +1,17 @@
+# Data Preprocessing Template For R
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+#Use below command once to install the required Package
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
